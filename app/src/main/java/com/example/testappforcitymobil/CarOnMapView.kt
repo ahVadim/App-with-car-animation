3533cbd12b63666d35c6131fee574eb80bd4cf41
v_ahtyamov <v.ahtyamov@onetrak.ru>
2019-07-13 16:16:47 +0300

    blue circle shifts to touch coordinates

--- app/src/main/java/com/example/testappforcitymobil/CarOnMapView.kt
+++ app/src/main/java/com/example/testappforcitymobil/CarOnMapView.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import kotlin.math.max
 
@@ -15,8 +16,10 @@ class CarOnMapView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    var mWidth: Int = 100
-    var mHeight: Int = 100
+    var mWidth: Float = 100F
+    var mHeight: Float = 100F
+    var currCarX: Float = 0F
+    var currCarY: Float = 0F
 
     val bluePaint = Paint().apply {
         color = Color.BLUE
@@ -28,15 +31,31 @@ class CarOnMapView @JvmOverloads constructor(
         val widthSpecSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightSpecSize = MeasureSpec.getSize(heightMeasureSpec)
 
-        mWidth = max(widthSpecSize, suggestedMinimumWidth)
-        mHeight = max(heightSpecSize, suggestedMinimumHeight)
+        val resultWidth = max(widthSpecSize, suggestedMinimumWidth)
+        val resultHeight = max(heightSpecSize, suggestedMinimumHeight)
 
-        setMeasuredDimension(mWidth, mHeight)
+        setMeasuredDimension(resultWidth, resultHeight)
+    }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        mWidth = w.toFloat()
+        mHeight = h.toFloat()
+        currCarX = mWidth/2
+        currCarY = mHeight/2
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            currCarX = it.x
+            currCarY = it.y
+            invalidate()
+        }
+
+        return true
     }
 
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawCircle(width/2.toFloat(), height/2.toFloat(), 20F, bluePaint)
+        canvas?.drawCircle(currCarX, currCarY, 20F, bluePaint)
     }
 
 }
