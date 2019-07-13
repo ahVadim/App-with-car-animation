70a657b1f6419f25f71b74433ec8bbca8c246847
Vadim Ahtyamov <datamod93@gmail.com>
2019-07-14 00:38:27 +0300

    функция просчета траектории

--- app/src/main/java/com/example/testappforcitymobil/CarOnMapView.kt
+++ app/src/main/java/com/example/testappforcitymobil/CarOnMapView.kt
@@ -4,15 +4,13 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
-import kotlin.math.max
+import kotlin.math.*
 
 class CarOnMapView @JvmOverloads constructor(
     context: Context,
@@ -23,6 +21,8 @@ class CarOnMapView @JvmOverloads constructor(
     var mWidth: Float = 100F
     var mHeight: Float = 100F
 
+    var radius: Float = 100F
+
     var oldCarX: Float = 0F
     var oldCarY: Float = 0F
 
@@ -31,9 +31,19 @@ class CarOnMapView @JvmOverloads constructor(
 
     var currCarX: Float = 0F
     var currCarY: Float = 0F
+    var currCarAngle: Float = 0F
+
+    var hintPath: Path = Path()
 
     val bluePaint = Paint().apply {
         color = Color.BLUE
+        isAntiAlias = true
+    }
+
+    val strokePaint = Paint().apply {
+        color = Color.RED
+        style = Paint.Style.STROKE
+        strokeWidth = 5F
         isAntiAlias = true
     }
 
@@ -79,13 +89,58 @@ class CarOnMapView @JvmOverloads constructor(
         if (event?.action == MotionEvent.ACTION_UP && !animator.isRunning){
             aimCarX = event.x
             aimCarY = event.y
-            animator.start()
+//            animator.start()
+            calculatePath()
         }
         return true
     }
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.drawCircle(currCarX, currCarY, 20F, bluePaint)
+        canvas?.drawPath(hintPath, strokePaint)
+    }
+
+    fun Float.toRadians(): Float {
+        return Math.toRadians(this.toDouble()).toFloat()
+    }
+
+    fun Float.toDegrees(): Float {
+        return Math.toDegrees(this.toDouble()).toFloat()
+    }
+
+    fun calculatePath(){
+
+        currCarAngle+=15F
+
+        val circleSign = sign(aimCarY - oldCarY - tan(currCarAngle.toRadians())*(aimCarX - oldCarX))*sign(cos(currCarAngle.toRadians()))
+
+        val rx = oldCarX + circleSign*radius* cos((PI/2 + currCarAngle.toRadians())).toFloat()
+        val ry = oldCarY + circleSign*radius* sin((PI/2 + currCarAngle.toRadians())).toFloat()
+
+        val startAngle = atan2(oldCarY - ry, oldCarX - rx).toDegrees()
+
+        val c = sqrt((rx-aimCarX).pow(2) + (ry-aimCarY).pow(2))
+
+        if (c < radius) {
+            hintPath.reset()
+            return
+        }
+
+        val b = sqrt(c.pow(2) - radius.pow(2))
+        val al = atan2(ry-aimCarY, rx - aimCarX)
+        val beta = asin(radius/c)
+
+        val cx = aimCarX + b* cos(al+beta*circleSign)
+        val cy = aimCarY + b* sin(al+beta*circleSign)
+        var endAngle = atan2(cy-ry, cx-rx).toDegrees()
+        if((endAngle-startAngle)*circleSign < 0) endAngle += (2*PI*circleSign).toFloat().toDegrees()
+
+        hintPath.reset()
+        val rect = RectF(rx - radius, ry - radius, rx + radius, ry + radius)
+        hintPath.addArc(rect, startAngle,endAngle-startAngle)
+        hintPath.moveTo(cx, cy)
+        hintPath.lineTo(aimCarX, aimCarY)
+        invalidate()
     }
 
 }
